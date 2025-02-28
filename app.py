b458995d3b27d7714142d9ad2fca06d8ceb71627
jakobgrodeland <97245890+jakobgrodeland@users.noreply.github.com>
2025-02-28 15:28:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__) 
 
-# Route for the home page (renders the form)
-@app.route("/", methods=["GET"])
-def home():
-    return render_template("CreateNewSet.html")  # Displays the create new set form page at the specified url
+# Route for the login page, which will be the start page. Note that dropping the methods parameter means only GET requests will be read, by default
+@app.route("/")
+def login():
+    # Displays the login page at the specified url, i.e /
+    return render_template("LoginPage.html")   
+
+# Route to the junction set design page from the home page
+@app.route("/setform")
+def setForm():
+    # Displays the junction set design page at the specified url, i.e setform
+    return render_template("CreateNewSet.html")
+
+# Route to get back to the home page from the junction set creation page
+@app.route("/home")
+def setFormToHome():
+    # Displays the home page at the url /home
+    return render_template("HomePage.html")
+
+
+# Route to handle login. Takes user to the home page
+@app.route("/home", methods=["POST"])
+def loginToHome():
+    # Get the username provided by the user
+    username = request.form.get("username")
+    print(username)
+    # Displays the create new set form page at the specified url
+    return render_template("HomePage.html")  
 
 
 @app.route('/save_junction', methods=['POST']) # The endpoint matches the figma button. This is when the user has finished designing their junction and wants to create a new one. 
@@ -17,10 +40,61 @@ def save_junction():
 
 # Route to handle the form submission (POST request)
 @app.route("/junctionform", methods=["POST"])
-def junction_form():
+def junctionForm():
     # Get the form data
     junctionSetName = request.form.get("junctionSetName")
     northVehiclesIn = request.form.get("northVehiclesIn")
+    # Checks to see whether there is a pedestrian crossing for each direction, and stores the yes/no value in the variable for each direction. yes = 1, no = 0.
+    northPedestrian = request.form.get("northPedestrian")
+    southPedestrian = request.form.get("southPedestrian")
+    eastPedestrian = request.form.get("eastPedestrian")
+    westPedestrian = request.form.get("westPedestrian")
+    # Gets the duration of the pedestrian crossings in seconds
+    crossingDuration = request.form.get("crossingDuration")
+
+    if northPedestrian is None:
+        northPedestrian = 0
+
+    if southPedestrian is None:
+        southPedestrian = 0
+
+    if eastPedestrian is None:
+        eastPedestrian = 0
+
+    if westPedestrian is None:
+        westPedestrian = 0
+
+    # Finds how many pedestrians are coming from each direction, and if there is no crossing, then we will set the number to -1 to differentiate between cases where we have 0 pedestrians. If a checkbox is unchecked, then the corresponding request frequency textbox will have a None value in Flask 
+    northRequestFrequency = request.form.get("northRequestFrequency")
+    southRequestFrequency = request.form.get("southRequestFrequency")
+    eastRequestFrequency = request.form.get("eastRequestFrequency")
+    westRequestFrequency = request.form.get("westRequestFrequency")
+
+    if northPedestrian == 0:
+        northRequestFrequency = -1
+    
+    if southPedestrian == 0:
+        southRequestFrequency = -1
+
+    if eastPedestrian == 0:
+        eastRequestFrequency = -1
+
+    if westPedestrian == 0:
+        westRequestFrequency = -1
+
+    print(northRequestFrequency)
+    print(southRequestFrequency)
+    print(eastRequestFrequency)
+    print(westRequestFrequency)
+
+    averageWaitPriority = request.form.get("AverageWait")
+    maximumWaitPriority = request.form.get("MaxWait")
+    maximumQueuePriority = request.form.get("MaxQueue")
+
+    print(averageWaitPriority)
+    print(maximumQueuePriority)
+    print(maximumWaitPriority)
+
     
     return render_template("LayoutDesignPage.html")
 
